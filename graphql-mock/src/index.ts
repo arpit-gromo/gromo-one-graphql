52743d18a72d28edd72a6106c032d4d707fb901e
Arpit Rai <arpit.rai@gromo.in>
2023-10-10 11:06:03 +0530

    Added Env PORT for deployment

--- graphql-mock/src/index.ts
+++ graphql-mock/src/index.ts
@@ -19,7 +19,7 @@ const server = new ApolloServer({
 //  3. prepares your app to handle incoming requests
 
 const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
+    listen: { port: Number.parseInt(process.env.PORT) || 4000 },
   });
   
   console.log(`🚀  Server ready at: ${url}`);
